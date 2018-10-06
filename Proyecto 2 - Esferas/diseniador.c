2a10696e665f1acee1d28e47e9a0e9c2a8e75c79
Edwin Cen <edwincx15@gmail.com>
2018-10-05 19:50:02 -0600

    planete

--- Proyecto 2 - Esferas/diseniador.c
+++ Proyecto 2 - Esferas/diseniador.c
@@ -59,6 +59,8 @@ int main(int argc, char** argv)
 	  // 	fprintf(inFile, v);
   	// }
 
+
+/*
     x =104;
     y = 333;
     z = 0;
@@ -172,12 +174,12 @@ int main(int argc, char** argv)
     }
 
 
+*/
 
 
 
 
-
-
+    
 
 
 
@@ -193,42 +195,204 @@ int main(int argc, char** argv)
     y = 233;
     z = 0;
 
-    for(int i = 0;i < 80;i++){
+    for(int i = 0;i < 90;i++){
 
-      if(i%10 ==0){
-        strcpy(v,"/////////////////\n#esfera\nradio:10\ncentro: (");
-      }
-      else{
-        strcpy(v,"/////////////////\n#esfera\nradio:5\ncentro: (");
-      }
+      int rad = 5 + rand()%5;
+      
+        strcpy(v,"/////////////////\n#esfera\nradio:");
+        sprintf(integ,"%d",rad);
+        strcat(v,integ);
+        strcat(v,"\ncentro: (");
+     
 
-      x =504+200*cos(grados);
+
+      //ojo:(804, 533, -1000)
+
+      x =504+220*cos(grados);
       z =200*sin(grados);
       
+      float xc = (1008)/ 2;
+      float yc = (567)/ 2;
+      int tempX;
+      int tempY;
+      tempX = x;
+      tempY = y;
+      double  tempXX = xc +(tempX - xc) * cos(1.7) - (tempY - yc)* sin(1.7);
+      double tempYY = yc + (tempX - xc) * sin(1.7) + (tempY - yc)* cos(1.7);
+
+
+      x = (int) tempXX;
+      y = (int) tempYY;
+
+      x += rand()%5;
+      y += rand()%5;
+      z += rand()%5;
+
       sprintf(integ,"%d,%d,%d",x,y,z);
       strcat(v,integ);
-      grados+=0.1;
+      grados+=0.4;
 
 
   
       strcat(v,")\ncolor:(");
 
-
-      if(i%10 ==0){
-        sprintf(integ,"0.%d,0.%d,0.%d",99,0,99);
+      int r = 53 +rand()% 20;
+      int g = 32 + rand()% 18;
+      int b = 11 + rand()% 24;
+      
+        sprintf(integ,"0.%d,0.%d,0.%d",r,g,b);
         strcat(v,integ);
-        strcat(v,")\nkd:1.0\nka:0.7\nks:0.8\nkn:1\n\n");
-      }
-      else{
-        sprintf(integ,"0.%d,0.%d,0.%d",0,0,0);
-        strcat(v,integ);
-        strcat(v,")\nkd:0.1\nka:0.1\nks:0.1\nkn:0.1\n\n");
-      }
+        strcat(v,")\nkd:1.0\nka:0.5\nks:0.0\nkn:1\n\n");
+      
+        
+      fprintf(inFile, v);
+    }
 
+    x =504;
+    y = 233;
+    z = 0;
+
+    for(int i = 0;i < 80;i++){
+
+      int rad = 5 + rand()%5;
+      
+        strcpy(v,"/////////////////\n#esfera\nradio:");
+        sprintf(integ,"%d",rad);
+        strcat(v,integ);
+        strcat(v,"\ncentro: (");
+     
+
+
+      //ojo:(804, 533, -1000)
+
+      x =504+200*cos(grados);
+      z =200*sin(grados);
+      
+      float xc = (1008)/ 2;
+      float yc = (567)/ 2;
+      int tempX;
+      int tempY;
+      tempX = x;
+      tempY = y;
+      double  tempXX = xc +(tempX - xc) * cos(1.7) - (tempY - yc)* sin(1.7);
+      double tempYY = yc + (tempX - xc) * sin(1.7) + (tempY - yc)* cos(1.7);
+
+
+      x = (int) tempXX;
+      y = (int) tempYY;
+
+      x += rand()%5;
+      y += rand()%5;
+      z += rand()%5;
+
+      sprintf(integ,"%d,%d,%d",x,y,z);
+      strcat(v,integ);
+      grados+=0.4;
+
+
+  
+      strcat(v,")\ncolor:(");
+
+      int r = 53 +rand()% 20;
+      int g = 32 + rand()% 18;
+      int b = 11 + rand()% 24;
+      
+        sprintf(integ,"0.%d,0.%d,0.%d",r,g,b);
+        strcat(v,integ);
+        strcat(v,")\nkd:1.0\nka:0.5\nks:0.0\nkn:1\n\n");
+      
+        
+      fprintf(inFile, v);
+    }
+
+for(int i = 0;i < 70;i++){
+
+      int rad = 5 + rand()%5;
+      
+        strcpy(v,"/////////////////\n#esfera\nradio:");
+        sprintf(integ,"%d",rad);
+        strcat(v,integ);
+        strcat(v,"\ncentro: (");
+     
+
+
+      //ojo:(804, 533, -1000)
+
+      x =504+240*cos(grados);
+      z =200*sin(grados);
+      
+      float xc = (1008)/ 2;
+      float yc = (567)/ 2;
+      int tempX;
+      int tempY;
+      tempX = x;
+      tempY = y;
+      double  tempXX = xc +(tempX - xc) * cos(1.7) - (tempY - yc)* sin(1.7);
+      double tempYY = yc + (tempX - xc) * sin(1.7) + (tempY - yc)* cos(1.7);
+
+
+      x = (int) tempXX;
+      y = (int) tempYY;
+
+      x += rand()%5;
+      y += rand()%5;
+      z += rand()%5;
+
+      sprintf(integ,"%d,%d,%d",x,y,z);
+      strcat(v,integ);
+      grados+=0.4;
+
+
+  
+      strcat(v,")\ncolor:(");
+
+      int r = 53 +rand()% 20;
+      int g = 32 + rand()% 18;
+      int b = 11 + rand()% 24;
+      
+        sprintf(integ,"0.%d,0.%d,0.%d",r,g,b);
+        strcat(v,integ);
+        strcat(v,")\nkd:1.0\nka:0.5\nks:0.0\nkn:1\n\n");
+      
+        
       fprintf(inFile, v);
     }
 
 
+
+z =1500;
+
+    for(int i = 0;i <180;i++){
+
+      int rad = rand()%5;
+      
+        strcpy(v,"/////////////////\n#esfera\nradio:");
+        sprintf(integ,"%d",rad);
+        strcat(v,integ);
+        strcat(v,"\ncentro: (");
+     
+
+
+      //ojo:(804, 533, -1000)
+
+      x =-2016+rand()%4032;
+      y =-1134+rand()%2268;
+
+      sprintf(integ,"%d,%d,%d",x,y,z);
+      strcat(v,integ);
+
+      strcat(v,")\ncolor:(");
+
+        sprintf(integ,"0.%d,0.%d,0.%d",99,99,99);
+        strcat(v,integ);
+        strcat(v,")\nkd:1.0\nka:0.9\nks:0.0\nkn:1\n\n");
+      
+        
+      fprintf(inFile, v);
+    }
+
+
+/*
     x =504;
     y = 233;
     z = 0;
@@ -300,7 +464,7 @@ int main(int argc, char** argv)
       strcat(v,")\nkd:1.0\nka:0.7\nks:0.8\nkn:1\n\n");
 
       fprintf(inFile, v);
-    }
+    }*/
     
 	return 0;
 }
